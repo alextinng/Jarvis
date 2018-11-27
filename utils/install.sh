fe1c0b60210798806781714bd1da4db5c8592a2c
AlexDing <aolong.ding@tiejin.me>
2018-11-27 14:44:22 +0800

    add script for installation and uninstallation.

---
+++ utils/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PREFIX="/bin"
+
+cp bin/rm.sh $PREFIX/safe-rm
+chmod 755 $PREFIX/safe-rm
+echo "Installation Succeeded!"
+echo "Please add \"alias rm='$PREFIX/safe-rm'\" to your ~/.bashrc script"
+echo "Enjoy!"
